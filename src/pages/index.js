052058460798a6e76a583ebce1f80e88a283da42
Suhas Mallesh <suhasmallesh@Suhass-MacBook-Pro.local>
2018-09-09 13:57:05 -0500

    Changed font

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>My name is Suhas Mallesh and I am a Web Developer👨🏻‍💻 | Travel Freak ✈️ | Foodie 😋</p>
+    <p>My name is <b>Suhas Mallesh</b> and I am a <b>Web Developer</b> 👨🏻‍💻 | <b>Travel Freak</b> ✈️ | <b>Foodie</b> 😋</p>
     <p>Vist my <a href="https://github.com/suhasm1990">Github</a> to view my projects</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
